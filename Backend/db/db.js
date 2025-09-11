8addddb0123700fffa0279f99d5b75add7bf6ace
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-09-11 21:12:51 +0530

    routes

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -11,7 +11,7 @@ const connectDB = async () => {
 
     console.log("✅ MongoDB connected successfully!");
   } catch (error) {
-    console.error("❌ MongoDB connection error:", error.message);
+    console.error("❌ MongoDB connection error: ", error.message);
     process.exit(1); 
   }
 };
