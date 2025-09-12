05aefee217609f61be363e5f12bd8f46fcb37a25
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-09-12 09:04:19 +0530

    hosting

---
+++ Backend/models/admin.js
@@ -0,0 +1,61 @@
+ const mongoose = require('mongoose')
+ const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+ const AdminSchema = new mongoose.Schema({
+    fullname: {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: [3, 'First name must be at least 3 characters long'],
+    },
+    lastname: {
+      type: String,
+      minlength: [3, 'Last name must be at least 3 characters long'],
+    }
+  },
+
+  email:{
+   type: String,
+    required: true,
+    unique: true,
+    minlength: [5, 'Email must be at least 5 characters'],
+  },
+password:{
+  type: String,
+    required: true,
+    select: false,
+} ,
+socketID: {
+    type: String,
+    default: null,
+  },
+
+   
+
+ })
+
+ 
+ AdminSchema.methods.generateAuthToken = function(){
+     const token = jwt.sign(
+         {_id: this._id},
+         process.env.JWT_SECRET,
+         {expiresIn: "24"}
+     )
+     return token
+    }
+ 
+ 
+    AdminSchema.method.comparePassword = async  function(password){
+      return await bcrypt.compare(password , this.password)
+    }
+ 
+    AdminSchema.statics.hashPassword = async function (password){
+     return await bcrypt.hash(password, 10)
+    }
+ 
+    const adminModel = mongoose.model('admin', AdminSchema)
+    module.exports = adminModel;
+    
+ 
